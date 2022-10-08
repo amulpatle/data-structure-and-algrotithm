deb75dadf5e0164519d5768aca23381572adcaca
amul patle <amulpatle3@gmail.com>
2022-10-08 05:37:59 +0530

    quetions

---
+++ Linked List/Problems of Linked List/Recursive Traversal in a Singly Linked List/RecursiveTraverse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    /* data */
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void RecursiveTraverse(Node *head)
+{
+    Node *curr = head;
+    if (curr == NULL)
+        return;
+
+    cout << curr->data << " ";
+    RecursiveTraverse(head->next);
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    head->next->next->next->next->next = new Node(60);
+    head->next->next->next->next->next->next = new Node(70);
+
+    RecursiveTraverse(head);
+
+    return 0;
+}
---
+++ Linked List/Problems of Linked List/Reverse a linked list in groups of size k/Reverse_K_Node.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    /* data */
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *Reverse_K_Node(Node *head, int k)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    Node *next = NULL;
+    Node *prev = NULL;
+    Node *curr = head;
+    int count = 0;
+    while (curr != NULL && count < k)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    if (next != NULL)
+    {
+        head->next = Reverse_K_Node(next, k);
+    }
+    return prev;
+}
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    head->next->next->next->next->next = new Node(60);
+    head->next->next->next->next->next->next = new Node(70);
+    printlist(head);
+    head = Reverse_K_Node(head, 3);
+    printlist(head);
+
+    return 0;
+}
