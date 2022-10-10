0effee4dd2503c9b0afb483e82150a50f162c9b3
amul patle <amulpatle3@gmail.com>
2022-10-11 00:30:21 +0530

    changes

--- Linked List/Problems of Linked List/Pairwise Swap Nodes of linked list in C++/changeLinks.cpp
+++ Linked List/Problems of Linked List/Pairwise Swap Nodes of linked list in C++/changeLinks.cpp
@@ -14,19 +14,45 @@ struct Node
 
 Node *Pairwiseswap(Node *head)
 {
-    Node *curr = head;
-    Node *prev = NULL;
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    Node *curr = head->next->next;
+    Node *prev = head;
+    head = head->next;
+    head->next = prev;
+
     while (curr != NULL && curr->next != NULL)
     {
-        Node *temp = curr->next->next;
-        curr->next->next = prev;
-        curr->next = temp;
-        prev->next = curr;
+        prev->next = curr->next;
+        prev = curr;
+        Node *next = curr->next->next;
+        curr->next->next = curr;
+        curr = next;
     }
+    prev->next = curr;
+    return head;
+}
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
 }
 
 int main()
 {
-
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    printlist(head);
+    head = Pairwiseswap(head);
+    printlist(head);
     return 0;
 }
