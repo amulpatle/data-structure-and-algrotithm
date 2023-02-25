bfc65965c21185cb5ddcd8dd5acab830f832734d
amul patle <amulpatle3@gmail.com>
2023-02-25 12:41:38 +0530

    Extract

--- Heap/Heapify and Extract/Heapify&Extract.cpp
+++ Heap/Heapify and Extract/Heapify&Extract.cpp
@@ -12,23 +12,38 @@ struct MinHeap
         arr = new int[c];
         size = c;
     }
-};
-void Heapify(int i)
-{
-    int lh = left(i);
-    int rh = right(i);
-    int smallest = i;
-    if (lh < size && arr[i] > arr[lh])
-        smallest = lh;
-    if (rh < size && arr[i] > arr[rh])
-        smallest = rh;
-
-    if (smallest != i)
+    void Heapify(int i)
     {
-        swap(arr[i], arr[smallest]);
-        Heapify(smallest);
+        int lh = left(i);
+        int rh = right(i);
+        int smallest = i;
+        if (lh < size && arr[i] > arr[lh])
+            smallest = lh;
+        if (rh < size && arr[i] > arr[rh])
+            smallest = rh;
+
+        if (smallest != i)
+        {
+            swap(arr[i], arr[smallest]);
+            Heapify(smallest);
+        }
     }
-}
+    int Extract()
+    {
+        if (size == 0)
+            return INT_MAX;
+        if (size == 1)
+        {
+            size--;
+            return arr[size];
+        }
+        swap(arr[0], arr[size - 1]);
+        size--;
+        Heapify(0);
+        return arr[size];
+    }
+};
+
 int main()
 {
     return 0;
