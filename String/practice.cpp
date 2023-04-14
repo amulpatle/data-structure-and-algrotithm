2702c77c6e5fa952d261b118c78fa259504c9865
amul patle <amulpatle3@gmail.com>
2023-04-14 11:35:54 +0530

    practice

---
+++ String/practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    char name[20];
+    cout << "Enter your name :";
+    cin >> name;
+    cout << "Your Name is : " << name << endl;
+    return 0;
+}
