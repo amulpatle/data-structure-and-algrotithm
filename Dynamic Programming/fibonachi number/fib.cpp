9b9587b9a3751311da5644037f957c78db8c6a9a
amul patle <amulpatle3@gmail.com>
2023-07-19 15:57:03 +0530

    fib Number using dp

---
+++ Dynamic Programming/fibonachi number/fib.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n, vector<int> &dp)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    else
+        return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a Number" << endl;
+    cin >> n;
+    vector<int> dp(n + 1, -1);
+    cout << f(n, dp);
+    return 0;
+}
